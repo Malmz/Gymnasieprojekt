a31346e96e7ee4bd52f8f94b75b21688452fa924
Calle Malmz <callem217@gmail.com>
2017-08-31 00:19:01 +0200

    Add Sprite class

---
+++ Gymnaieprojekt/Sprite.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace Gymnaieprojekt
+{
+    class Sprite
+    {
+        private Texture2D texture;
+        private Vector2 position;
+        private Vector2 scale;
+        private Rectangle srcRect;
+        private Color color;
+
+        public Sprite(Texture2D texture, Vector2 position, Vector2 size, Color? color, Rectangle? srcRect)
+        {
+            this.texture = texture;
+            this.position = position;
+            scale = size/texture.Bounds.Size.ToVector2();
+            if (srcRect != null)
+            {
+                this.srcRect = (Rectangle)srcRect;
+            }
+            else
+            {
+                this.srcRect = texture.Bounds;
+            }
+            if (color != null)
+            {
+                this.color = (Color)color;
+            }
+            else
+            {
+                this.color = Color.White;
+            }
+            
+        }
+
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            spriteBatch.Draw(
+                texture: texture,
+                position: position,
+                scale: scale,
+                sourceRectangle: srcRect,
+                color: Color.White,
+                rotation: 0,
+                origin: Vector2.Zero,
+                effects: SpriteEffects.None,
+                layerDepth: 0
+            );
+        }
+    }
+}
