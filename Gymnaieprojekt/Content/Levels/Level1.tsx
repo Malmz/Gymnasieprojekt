106c73a0ac8d767b08f6f233b374f770169ce375
Calle Malmz <callem217@gmail.com>
2017-10-04 22:02:53 +0200

    Add level1 class

---
+++ Gymnaieprojekt/Content/Levels/Level1.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="Colorful" tilewidth="64" tileheight="64" tilecount="264" columns="22">
+ <image source="../Visual Studio 2017/Projects/Gymnasieprojekt/Gymnaieprojekt/Content/tilesheet_complete.png" width="1408" height="768"/>
+</tileset>
---
+++ Gymnaieprojekt/GameState/States/Level1.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using MonoGame.Extended.Graphics;
+using MonoGame.Extended.Tiled;
+
+namespace Gymnaieprojekt.GameState.States
+{
+    class Level1 : GameStateBase
+    {
+        private TiledMapRenderer mapRenderer;
+        private TiledMap map;
+
+        protected Level1(Context context) : base(context)
+        {
+            mapRenderer = new TiledMapRenderer(Context.GraphicsDevice);
+            map = Context.Content.Load<TiledMap>("testmap");
+            foreach (var val in map.ImageLayers)
+            {
+                val.
+            }
+        }
+
+        public override void Draw(GameTime gameTime, SpriteBatch spriteBatch)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public override void Update(GameTime gameTime, GameStateManager stateManager)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
