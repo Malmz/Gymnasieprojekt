cc82f1d06a9301e85d434991beefa0179e072580
MechaKnightz <MechaKnightz@hotmail.com>
2017-10-10 12:34:16 +0200

    added player and subsprites

--- Gymnaieprojekt/Players/Player.cs
+++ Gymnaieprojekt/Players/Player.cs
@@ -1,9 +1,49 @@
-﻿using Gymnaieprojekt.Sprites;
+﻿using System.Collections.Generic;
+using Gymnaieprojekt.Sprites;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+// ReSharper disable InconsistentNaming
 
 namespace Gymnaieprojekt.Players
 {
     public class Player
     {
-        private AnimatedSprite animatedSprite;
+        private readonly AnimatedSprite sprite;
+        private readonly List<PlayerSubSprite> sprites;
+
+        public Player(AnimatedSprite sprite)
+        {
+            sprites = new List<PlayerSubSprite>();
+            this.sprite = sprite;
+        }
+
+        public void Update(GameTime gameTime)
+        {
+            sprite.Update(gameTime);
+            foreach (var subSprite in sprites)
+            {
+                subSprite.Update(gameTime);
+                subSprite.X(sprite.X + subSprite.offset.X);
+                subSprite.Y(sprite.Y + subSprite.offset.Y);
+            }
+        }
+
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            sprite.Draw(spriteBatch);
+            foreach (var subSprite in sprites)
+            {
+                if (!subSprite.isVisible) return;
+                subSprite.Draw(spriteBatch);
+            }
+        }
+
+        public void AddSubSprite(Sprite sprite, Vector2? offset = null, bool isVisible = true)
+        {
+            var off = Vector2.Zero;
+            if (offset != null) off = offset.Value;
+
+            sprites.Add(new PlayerSubSprite(sprite, off, isVisible));
+        }
     }
 }
